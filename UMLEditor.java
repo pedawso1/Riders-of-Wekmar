a672e09a68b2233f277754ad2ced4d5de7a21c80
KyleMcStyle <31778936+KyleMcStyle@users.noreply.github.com>
2017-09-19 18:04:26 -0400

    Update UMLEditor.java
    Implemented the line tool for canvas which snaps every 25px, rather than only to each corner of the grid (every 50px).

--- UMLEditor.java
+++ UMLEditor.java
@@ -3,7 +3,6 @@
  */
 package umleditor;
 
-import static com.sun.javafx.fxml.expression.Expression.and;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -30,6 +29,7 @@ import javafx.stage.Stage;
 public class UMLEditor extends Application {
 
     int x, y;
+    int cornerX, cornerY;
 
     @Override
     public void start(Stage primaryStage) {
@@ -52,21 +52,21 @@ public class UMLEditor extends Application {
         //Draw a line
         //Line line1 = new Line(0, 100, 100, 0);
         //grid.add(line1, 0, 1);
+        
         //Buttons
         ToggleGroup tg = new ToggleGroup();
-        //ToggleButton lineButton = new ToggleButton("Line Tool");    
+        ToggleButton lineButton = new ToggleButton("Line Tool");    
         ToggleButton selectButton = new ToggleButton("Move Tool");
         ToggleButton squareButton = new ToggleButton("Square Button");
         ToggleButton delSquareButton = new ToggleButton("Delete Square Button");
         //tg.getToggles().addAll(selectButton, squareButton, delSquareButton);
-        //lineButton.setToggleGroup(tg);
+        lineButton.setToggleGroup(tg);
         selectButton.setToggleGroup(tg);
         squareButton.setToggleGroup(tg);
         delSquareButton.setToggleGroup(tg);
         Button clearButton = new Button("Clear");
         VBox buttonBox = new VBox(15);
-        //buttonBox.getChildren().add(lineButton);
-        buttonBox.getChildren().addAll(selectButton, squareButton, delSquareButton, clearButton);
+        buttonBox.getChildren().addAll(lineButton, selectButton, squareButton, delSquareButton, clearButton);
         buttonBox.setAlignment(Pos.CENTER);
         grid.add(buttonBox, 0, 0);
 
@@ -79,15 +79,23 @@ public class UMLEditor extends Application {
         gc.fillRect(0, 0, 50 * columns, 50 * rows);
         drawGrid(gc, columns, rows);
         grid.add(canvas, 1, 0);
-
+        
+        //bool array of squares created by square tool
         boolean[][] isOccupied = new boolean[columns][rows];
 
         //Mouse events
         EventHandler<MouseEvent> mouseEventHandler = (MouseEvent e) -> {
             if (e.getEventType() == MouseEvent.MOUSE_PRESSED) {
-                x = (int) e.getX() / 50;
-                y = (int) e.getY() / 50;
-                System.out.println(x + " , " + y);
+                int gotX = (int) e.getX();
+                int gotY = (int) e.getY();
+                x = (int) gotX / 50;
+                y = (int) gotY / 50;
+                if (gotX % 25 < 13){cornerX = gotX - gotX % 25;}
+                else {cornerX = gotX + (25 - gotX % 25);}
+                if (gotY % 25 < 13){cornerY = gotY - gotY % 25;}
+                else {cornerY = gotY + (25 - gotY % 25);}
+                //System.out.println(x + " , " + y);
+                //System.out.println(cornerX + " , " + cornerY);
             } else if (e.getEventType() == MouseEvent.MOUSE_CLICKED) {
                 if (squareButton.isSelected()) {
                     gc.setFill(Color.BLACK);
@@ -106,7 +114,17 @@ public class UMLEditor extends Application {
                     }
                 }
             } else if (e.getEventType() == MouseEvent.MOUSE_RELEASED) {
-                if (selectButton.isSelected()) {
+                if (lineButton.isSelected()) {
+                    int curGotX = (int) e.getX();
+                    int curGotY = (int) e.getY();
+                    int curCornerX;
+                    int curCornerY;
+                    if (curGotX % 25 < 13){curCornerX = curGotX - curGotX % 25;}
+                    else {curCornerX = curGotX + (25 - curGotX % 25);}
+                    if (curGotY % 25 < 13){curCornerY = curGotY - curGotY % 25;}
+                    else {curCornerY = curGotY + (25 - curGotY % 25);}
+                    gc.strokeLine(cornerX, cornerY, curCornerX, curCornerY);
+                } else if (selectButton.isSelected()) {
                     if (isOccupied[x][y]) {
                         gc.setFill(Color.BLACK);
                         gc.fillRect(e.getX() - e.getX() % 50, e.getY() - e.getY() % 50, 50, 50);
@@ -128,7 +146,6 @@ public class UMLEditor extends Application {
             gc.fillRect(0, 0, 50 * columns, 50 * rows);
             drawGrid(gc, columns, rows);
         });
-
 
         //outputMouseCoords(grid);
 
