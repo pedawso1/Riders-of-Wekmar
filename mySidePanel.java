612a096f0a2a084d281973a56ce6b3c32e5daee5
acamacho92 <31419639+acamacho92@users.noreply.github.com>
2017-10-18 20:00:56 -0400

    Update mySidePanel.java

--- mySidePanel.java
+++ mySidePanel.java
@@ -1,5 +1,4 @@
-package application;
-
+package RidersOfWekmar;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -10,96 +9,83 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
-
+//Creates a side panel to house the buttons to create the TextClassBoxes and drawLines 
+//to form the UML diagram
 public class mySidePanel {
 
-    GridPane grid = new GridPane();
-    TextBoxClass hold = new TextBoxClass();
-    ToggleButton line = new ToggleButton();
-    ToggleButton boxwa = new ToggleButton();
-    
-    public GridPane addSidePanel() {
-		
+	GridPane grid = new GridPane();
+	ToggleButton select = new ToggleButton();
+	ToggleButton line = new ToggleButton();
+	ToggleButton delete = new ToggleButton();
+	ToggleButton boxwa = new ToggleButton();
+	Pane centerPane = new Pane();
+
+	// Force centerPane integration to reduce main class clutter
+	public mySidePanel(Pane cp) {
+		centerPane = cp;
+	}
+
+	public GridPane addSidePanel() {
+
 		grid.setAlignment(Pos.TOP_LEFT);
 		grid.setHgap(10);
 		grid.setVgap(10);
-                //is this supposed to be 0? (insets are top, left, bottom, right)
 		grid.setPadding(new Insets(10, 10, 0, 10));
-                
-                ToggleGroup tg = new ToggleGroup();
+
+		ToggleGroup tg = new ToggleGroup();
 
 		/*
-		 * Select button with icon
+		 * Delete button with icon
 		 */
-		Image imageSelect = new Image(getClass().getResourceAsStream("select.png"));
-		ImageView imageView = new ImageView(imageSelect);
-		imageView.setFitWidth(30);
-		imageView.setFitHeight(30);
-		
-		Button select = new Button();
-		select.getStyleClass().add("delete");
-		select.setGraphic(new ImageView(imageSelect));
-		select.setGraphic(imageView);
-		
-		HBox hbBtn = new HBox();
-		hbBtn.setAlignment(Pos.TOP_LEFT);
-		hbBtn.getChildren().add(select);
-		grid.add(hbBtn, 0, 0);
+		Image imageDelete = new Image(getClass().getResourceAsStream("delete.png"));
+		ImageView imageView = new ImageView(imageDelete);
+		delete.getStyleClass().add("delete");
+		delete.setToggleGroup(tg);
+		delete.setGraphic(new ImageView(imageDelete));
+		delete.setAlignment(Pos.CENTER);
+		grid.add(delete, 0, 0);
 
-		/*
-		 * Box button with icon
-		 */
-	
-
-		/*
-		 * Line button with icon
-		 */
-		Image imageLine = new Image(getClass().getResourceAsStream("drawLine.png"));
-	        line.getStyleClass().add("line");
-                line.setToggleGroup(tg);
-		line.setGraphic(new ImageView(imageLine));
-                line.setAlignment(Pos.CENTER);
-		grid.add(line, 0, 2);
+		// TextBox button
+		Image imageBox = new Image(getClass().getResourceAsStream("textBox.png"));
+		Button textBox = new Button();
+		textBox.setGraphic(new ImageView(imageBox));
+		textBox.setAlignment(Pos.CENTER);
+		grid.add(textBox, 0, 1);
+		// setting click event to spawn box
+		textBox.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+			TextBoxClass hold = new TextBoxClass(this);
+			hold.spawn(centerPane);
+		});
 
 		/*
 		 * Text button
 		 */
 		Button text = new Button("Text");
 		text.getStyleClass().add("text");
-                text.setAlignment(Pos.CENTER);
-                grid.add(text, 0, 3);
-                
-                
+		text.setAlignment(Pos.CENTER);
+		grid.add(text, 0, 2);
 
 
+		/*
+		 * Line button with icon
+		 */
+		Image imageLine = new Image(getClass().getResourceAsStream("drawLine.png"));
+		line.getStyleClass().add("line");
+		line.setToggleGroup(tg);
+		line.setGraphic(new ImageView(imageLine));
+		line.setAlignment(Pos.CENTER);
+		grid.add(line, 0, 3);
 		return grid;
 	}
-    
-    public boolean lineBtnToggled(){
-        return line.isSelected();
-    }
-      public boolean boxBtnToggled(){
-        return boxwa.isSelected();
-    }
-      
-     public Pane box(Pane pane)
-     {
-          	Image imageBox = new Image(getClass().getResourceAsStream("square.png"));
-		Button box = new Button();
-		box.setGraphic(new ImageView(imageBox));
-                box.setAlignment(Pos.CENTER);
-		grid.add(box, 0, 1);
-                
-                	 box.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> 
-                 {
-                           TextBoxClass hold = new TextBoxClass();
-                           hold.spawn(pane);
-             
 
-		 });
-                return pane;
-     }
+	// setting the line button boolean toggle
+	public boolean lineBtnToggled() {
+		return line.isSelected();
+	}
+
+	public boolean deleteBtnToggled() {
+		return delete.isSelected();
+	}
 }
