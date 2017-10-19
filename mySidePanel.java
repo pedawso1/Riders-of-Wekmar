f0c4831d2393a035d72705700a8044b5878c4617
acamacho92 <31419639+acamacho92@users.noreply.github.com>
2017-10-18 20:02:36 -0400

    Update mySidePanel.java

--- mySidePanel.java
+++ mySidePanel.java
@@ -1,7 +1,5 @@
 package RidersOfWekmar;
 
-
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -11,102 +9,82 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 //Creates a side panel to house the buttons to create the TextClassBoxes and drawLines 
 //to form the UML diagram
-public class mySidePanel 
-{
+public class mySidePanel {
 
-    GridPane grid = new GridPane();
-    ToggleButton select = new ToggleButton();
-    ToggleButton line = new ToggleButton();
-    ToggleButton delete = new ToggleButton("delete");
-    ToggleButton boxwa = new ToggleButton();
-    Pane centerPane = new Pane();
-    
-    
-    //Force centerPane integration to reduce main class clutter
-    public mySidePanel(Pane cp){
-        centerPane = cp;
-    }
-    
-    public GridPane addSidePanel() 
-    {
-	
+	GridPane grid = new GridPane();
+	ToggleButton select = new ToggleButton();
+	ToggleButton line = new ToggleButton();
+	ToggleButton delete = new ToggleButton();
+	ToggleButton boxwa = new ToggleButton();
+	Pane centerPane = new Pane();
+
+	// Force centerPane integration to reduce main class clutter
+	public mySidePanel(Pane cp) {
+		centerPane = cp;
+	}
+
+	public GridPane addSidePanel() {
+
 		grid.setAlignment(Pos.TOP_LEFT);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(10, 10, 0, 10));
-                
-                ToggleGroup tg = new ToggleGroup();
+
+		ToggleGroup tg = new ToggleGroup();
 
 		/*
-		 * Select button with icon
+		 * Delete button with icon
 		 */
-                
-		Image imageSelect = new Image(getClass().getResourceAsStream("icons/select.png"));
-		ImageView imageView = new ImageView(imageSelect);
-		imageView.setFitWidth(30);
-		imageView.setFitHeight(30);
-		select.getStyleClass().add("select");
-                select.setToggleGroup(tg);
-		select.setGraphic(new ImageView(imageSelect));
-		select.setGraphic(imageView);
-                
-		grid.add(select, 0, 0);
-                
-                
-                //TextBox button
-		Image imageBox = new Image(getClass().getResourceAsStream("icons/textBox.png"));
+		Image imageDelete = new Image(getClass().getResourceAsStream("delete.png"));
+		delete.getStyleClass().add("delete");
+		delete.setToggleGroup(tg);
+		delete.setGraphic(new ImageView(imageDelete));
+		delete.setAlignment(Pos.CENTER);
+		grid.add(delete, 0, 0);
+
+		// TextBox button
+		Image imageBox = new Image(getClass().getResourceAsStream("textBox.png"));
 		Button textBox = new Button();
 		textBox.setGraphic(new ImageView(imageBox));
-                textBox.setAlignment(Pos.CENTER);
+		textBox.setAlignment(Pos.CENTER);
 		grid.add(textBox, 0, 1);
-		//setting click event to spawn box 
-                textBox.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> 
-                {
-                    TextBoxClass hold = new TextBoxClass(this);
-                    hold.spawn(centerPane);
-		});                
-                            
-                
-		/*
-		 * Line button with icon
-		 */
-		Image imageLine = new Image(getClass().getResourceAsStream("icons/drawLine.png"));
-	        line.getStyleClass().add("line");
-                line.setToggleGroup(tg);
-		line.setGraphic(new ImageView(imageLine));
-                line.setAlignment(Pos.CENTER);
-		grid.add(line, 0, 2);
+		// setting click event to spawn box
+		textBox.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+			TextBoxClass hold = new TextBoxClass(this);
+			hold.spawn(centerPane);
+		});
 
 		/*
 		 * Text button
 		 */
 		Button text = new Button("Text");
 		text.getStyleClass().add("text");
-                text.setAlignment(Pos.CENTER);
-                grid.add(text, 0, 3);
-                
-                //Delete button
-		delete.getStyleClass().add("delete");
-                delete.setToggleGroup(tg);
-                delete.setAlignment(Pos.CENTER);
-                grid.add(delete, 0, 4);
-                
+		text.setAlignment(Pos.CENTER);
+		grid.add(text, 0, 2);
+
+
+		/*
+		 * Line button with icon
+		 */
+		Image imageLine = new Image(getClass().getResourceAsStream("drawLine.png"));
+		line.getStyleClass().add("line");
+		line.setToggleGroup(tg);
+		line.setGraphic(new ImageView(imageLine));
+		line.setAlignment(Pos.CENTER);
+		grid.add(line, 0, 3);
 		return grid;
 	}
-    
-    	 //setting the line button boolean toggle 
-         public boolean lineBtnToggled()
-  	 {
-      	        return line.isSelected();
- 	 }
-         
-         public boolean deleteBtnToggled()
-         {
-             return delete.isSelected();
-         }
+
+	// setting the line button boolean toggle
+	public boolean lineBtnToggled() {
+		return line.isSelected();
+	}
+
+	public boolean deleteBtnToggled() {
+		return delete.isSelected();
+	}
 }
