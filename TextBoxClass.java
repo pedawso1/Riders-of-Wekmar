18f3f88287cce42a0aefb504ab296edd0b642623
knsayal <31737158+knsayal@users.noreply.github.com>
2017-11-05 22:20:25 -0500

    Update TextBoxClass.java

--- TextBoxClass.java
+++ TextBoxClass.java
@@ -4,10 +4,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
 */
- package RidersOfWekmar;
-
+ 
 import static javafx.application.Application.launch;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -48,39 +48,129 @@ public TextBoxClass (mySidePanel sidePanel)
 //Returns a box with a box for moving at the top and three text boxes below it, formated via setLayout
  public Pane spawn(Pane box)
  {
+        Button grow = new Button("+  ");
+	  
+	 
+	   Button shrink = new Button("-   ");
+	   shrink.setLayoutX(30);
+	 
+	 
+	 
+	 Button setting = new Button("..");
+	 setting.setLayoutX(60);
 	 //creating box to grab for TextBox movement
 	          classBox = new Rectangle();
-                  classBox.setLayoutX(0);
+                  classBox.setLayoutX(70);
                   classBox.setLayoutY(0);
-                  classBox.setWidth(150);
+                  classBox.setWidth(80);
                   classBox.setHeight(25);
 		
 	 //Text fields for Class,Attributes, Instance Variables
-		TextField text1 = new TextField("Class Name");
-	 	text1.setMaxWidth(150);
-		text1.setLayoutY(25);
-		
-                TextArea text3 = new TextArea("Attributes!!!!");
-		text3.setMaxSize(200, 75);
-		text3.setMaxWidth(150);
-		text3.setLayoutY(50);
-		
-		TextArea text2 = new TextArea("Instance Varibles");
-		text2.setMaxSize(200, 100);
-		text2.setMaxWidth(150);
-		text2.setLayoutY(125);
-		
+                  TextArea text1 = new TextArea("Class Name");
+          		text1.setMaxSize(150, 25);
+          		//text1.setMaxWidth(300);
+          		//text1.setMaxHeight(25);
+          		text1.setLayoutY(25);
+          		
+                 
+          		TextArea text2 = new TextArea("Instance Varibles");
+          		  
+          		text2.setMaxHeight(100);
+          		text2.setMaxWidth(150);
+          		text2.setLayoutY(125);
+          		
+          		
+          		TextArea text3 = new TextArea("Attributes!!!!");
+          		
+          		text3.setMaxWidth(150);
+          		text3.setMaxHeight(75);
+          		text3.setLayoutY(50);
+          		
+          		
                
                 
 		Pane group = new Pane();
 		StackPane stackp = new StackPane();
 		stackp.getChildren().add(group);
 		box.getChildren().add(group);
-		group.getChildren().addAll(text1,text2,text3,classBox);
+		group.getChildren().addAll(text1,text2,text3,classBox,grow,shrink,setting);
 		
 		group.setId("Text Box");
+		///////////////////////////////////////////////////////////////////////////////
 		
+         grow.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+			
+			if(text2.getHeight()<190){
+			 classBox.setWidth(classBox.getWidth()+5);
+			    text1.setMaxWidth(text1.getMaxWidth()+5);
+				
+			   
+				
+				
+				text2.setMaxWidth(text2.getMaxWidth()+5);
+				text2.setMaxHeight(text2.getMaxHeight()+5);
+				
+				text3.setMaxWidth(text3.getMaxWidth()+5);
+				text3.setMaxHeight(text3.getMaxHeight()+5);
+				
+				
+				
+				text2.setLayoutY(text2.getLayoutY()+5);
+				
+			}
+         });
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+         shrink.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+ 			
+ 			if(text2.getHeight()>110){
+ 			    classBox.setWidth(classBox.getWidth()-5);
+ 			    text1.setMaxWidth(text1.getMaxWidth()-5);
+ 				
+ 			   
+ 				
+ 				
+ 				text2.setMaxWidth(text2.getMaxWidth()-5);
+ 				text2.setMaxHeight(text2.getMaxHeight()-5);
+ 				
+ 				text3.setMaxWidth(text3.getMaxWidth()-5);
+ 				text3.setMaxHeight(text3.getMaxHeight()-5);
+ 				
+ 				
+ 				text2.setLayoutY(text2.getLayoutY()-5);
+ 			
+ 		}
+ 				
+ 				
+ 		
+ 			
+ 		});
+///////////////////////////////////////////////////////////////////////////////////////////
+         setting.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+  		   
+ 			
+	         classBox.setWidth(80);
+             classBox.setHeight(25);
+	
+	
+	      text1.setMaxSize(150, 25);
 		
+	      text2.setMaxHeight(100);
+	      text2.setMaxWidth(150);
+	      text2.setLayoutY(125);
+	
+	
+	      text3.setMaxWidth(150);
+	      text3.setMaxHeight(75);
+	
+	
+			
+			
+			
+			
+			
+			
+		});
+		         
 		
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 	 
