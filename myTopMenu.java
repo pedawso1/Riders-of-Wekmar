06be04462e861a194a4d4ac1d7393419c9932a02
pedawso1 <31413066+pedawso1@users.noreply.github.com>
2017-11-07 10:48:07 -0500

    Update myTopMenu.java

--- myTopMenu.java
+++ myTopMenu.java
@@ -17,46 +17,51 @@ import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-/**
- * An object to add file options through drop down menus * 
- */
-public class myTopMenu {
+//myTopMenu is an object to add file options through drop down menus
+public class myTopMenu 
+{
 
     MenuBar menuBar = new MenuBar();
     ArrayList<Node> nodes = new ArrayList<Node>();
     TextBoxClass textBox;
     String addingToFile = "";
     
-    /**
-     * Create menu bar with save functionality
-     * @param primary PrimaryStage from RidersOfWekmar class
-     * @param centerPane centerPane from RidersOfWekmar class
-     * @return return the menu bar itself
-     */
-    public MenuBar addMenuBar(Stage primary, Pane centerPane) {    
+    
+    public MenuBar addMenuBar(Stage primary, Pane centerPane) 
+    {    
         javafx.scene.control.Menu menuFile = new javafx.scene.control.Menu("File");
         MenuItem newFile = new MenuItem("New");
         MenuItem open = new MenuItem("Open");
         MenuItem save = new MenuItem("Save");
         
-        save.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent t) {
+        save.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            //We have a functional save aspect that will store node type and location to a 
+            //text file, load function is planned but not functional
+            public void handle(ActionEvent t) 
+            {
                 FileChooser fc = new FileChooser();
                 fc.setTitle("Save Text");
                 nodes = getAllNodes(centerPane);
-                for (int i = 0; i < nodes.size(); i++) {
+                
+                for (int i = 0; i < nodes.size(); i++) 
+                {
                 	ArrayList<Double> list = new ArrayList<Double>();
                 	list = getBounds(nodes.get(i));
                 	System.out.println(i + " " + nodes.get(i).getId() + " " + list);
                 	addingToFile = addingToFile + (i + " " + nodes.get(i).getId() + " " + list).toString() + "\n";
                 	
                 }
+                
                 FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
                 fc.getExtensionFilters().add(extFilter);
                 File file = fc.showSaveDialog(primary);
-                if (file != null) {
+                
+                if (file != null) 
+                {
                     SaveFile(addingToFile , file);
                 }
+                
                 addingToFile = "";
             }
         });  
@@ -74,39 +79,65 @@ public class myTopMenu {
         return menuBar;
            
     }
-
-    private void SaveFile(String content, File file){
-        try {
+    
+    private void SaveFile(String content, File file)
+    {
+        try 
+        {
             FileWriter fileWriter = null;
              
             fileWriter = new FileWriter(file);
             fileWriter.write(content);
             fileWriter.close();
-        } catch (IOException ex) {
+        } catch (IOException ex) 
+        {
             Logger.getLogger(myTopMenu.class.getName()).log(Level.SEVERE, null, ex);
         }
          
     }
-    public static ArrayList<Double> getBounds(Node node) {
+    
+    //Fuction to return Arraylist of only top left corner of nodes
+    //intended use is for snapping Lines to text boxes within a predetermined
+    //proximity, currently not used
+      public static ArrayList<Double> getMinBounds(Node node) 
+      {
     	Bounds bounds;
     	ArrayList<Double> list = new ArrayList<Double>();
     	bounds = node.localToParent(node.getBoundsInLocal());
     	list.add(bounds.getMinX());
+        list.add(bounds.getMinY());
+    	return list;
+    }
+    
+    //Returns the bounds for all nodes in scene, returning both the min
+    //and max X and Y coordinates too record both sides of lines and
+    //also because we can resize our text boxes we need the diagonal coordinates
+    // to determine location and size for save
+    public static ArrayList<Double> getBounds(Node node) 
+    {
+    	Bounds bounds;
+    	ArrayList<Double> list = new ArrayList<Double>();
+    	bounds = node.localToParent(node.getBoundsInLocal());
+    	list.add(bounds.getMinX());
+        list.add(bounds.getMinY());
     	list.add(bounds.getMaxX());
-    	list.add(bounds.getMinY());
     	list.add(bounds.getMaxY());
     	return list;
     }
     
-    public static ArrayList<Node> getAllNodes(Parent root) {
+    //Returns all nodes from "root" and descendents
+    public static ArrayList<Node> getAllNodes(Parent root) 
+    {
         ArrayList<Node> nodes = new ArrayList<Node>();
         addAllDescendents(root, nodes);
         return nodes;
     }
 
-    
-    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes) {
-        for (Node node : parent.getChildrenUnmodifiable()) {
+    //Returns all descendents from parent node
+    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes)
+    {
+        for (Node node : parent.getChildrenUnmodifiable()) 
+        {
             nodes.add(node);
             //if (node instanceof Parent)
             //    addAllDescendents((Parent)node, nodes);
