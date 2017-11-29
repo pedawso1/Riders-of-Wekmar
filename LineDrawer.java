db70395db04d4c6bfe57005f6986ebb06154b55d
KyleMcStyle <31778936+KyleMcStyle@users.noreply.github.com>
2017-11-29 09:48:53 -0500

    adding settings to topmenu

---
+++ LineDrawer.java
@@ -0,0 +1,227 @@
+package RidersOfWekmar;
+
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+
+
+
+/**
+ * Independent class for handling lines and their creation on centerPane. Line 
+ * endpoints snap to a 10x10 grid.
+ */
+public class LineDrawer 
+{
+    double strokeWidth;
+    double triangleHeight, triangleWidth;
+    double x1, y1, x2, y2;
+    Pane centerPane;
+    mySidePanel sidePanel;
+    /**
+     * Current lineTypes:
+     * 1: Plain line
+     * 2: Line with arrow
+     * 3: Dependency (dotted) line
+     */
+    int lineType;
+    
+
+    /**
+     * Initialize the class so that components of the class may check sidePanel 
+     * toggles and perform actions on the centerPane
+     * @param sp application's side panel
+     * @param centerPane application's center centerPane
+     */
+    public LineDrawer(mySidePanel sp) 
+    {
+        strokeWidth = 3;
+        triangleHeight = 20;
+        triangleWidth = 40;
+        centerPane = sp.getCenterPane();
+        centerPane.setOnMousePressed(press);
+        centerPane.setOnMouseReleased(release);
+        //lineStack = new Stack<>();
+        centerPane = centerPane;
+        sidePanel = sp;
+    }
+
+    /**
+     * Create new line object and add it as a child to the center centerPane after the
+     * coordinates have been acquired.
+     */
+    private void drawLine() 
+    {
+        Line l = new Line(x1, y1, x2, y2);
+        l.setStrokeWidth(strokeWidth);
+        if (lineType == 1)
+        {
+            pushLine(l);
+        } else if (lineType == 2)
+        {
+            drawArrowLine(l);
+        } else if (lineType == 3)
+        {
+            drawDashedLine(l);
+        }
+    }
+    
+    private void drawArrowLine(Line l) 
+    {
+        //Line head logic        
+        double angleRadians = Math.atan2(y2 - y1, x2 - x1);
+        double angleDegrees = angleRadians * (180 / Math.PI);
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+                0.0, 0.0,
+                0.0, triangleWidth,
+                triangleHeight, triangleWidth / 2
+        );
+        triangle.setTranslateX(x2 - triangleHeight / 2);
+        triangle.setTranslateY(y2 - triangleWidth / 2);
+        triangle.setRotate(angleDegrees);
+        
+        Group g = new Group();
+        g.getChildren().addAll(l, triangle);
+        pushLine(g);
+    }
+    
+    private void drawDashedLine(Line l)
+    {
+        l.getStrokeDashArray().addAll(10d, 7d);
+        pushLine(l);
+    }
+    
+    private void pushLine(Node l)
+    {
+        l.setOnMouseClicked(delete);
+        l.setId("Line");
+        l.toBack();
+        sidePanel.pushToUndoStack(l);
+        centerPane.getChildren().add(l);  
+    }
+    
+    
+    /**
+     * Undo the creation the last line (delete last created line)
+     * Line stack not currently used
+     */
+    /*
+    public void undo()
+    {
+        if (!lineStack.empty())
+        {
+            Line l = lineStack.pop();
+            centerPane.getChildren().remove(l);
+        }
+    }
+    */
+    
+    /**
+     * Prototype function for drawing right-angle lines (WIP)
+     */
+    /*
+    private void drawRightAngleLine()
+    {
+        Line l1 = new Line(x1, y1, x2, y1);
+        Line l2 = new Line(x2, y1, x2, y2);
+        l1.setStrokeWidth(3);
+        l1.setOnMouseClicked(delete);
+        l2.setStrokeWidth(3);
+        l2.setOnMouseClicked(delete);
+        centerPane.getChildren().addAll(l1, l2);
+    }
+    */
+
+    /**
+     * Remove passed in Line object as a child of center centerPane
+     * @param l 
+     */
+    public void delete(Line l)
+    {  
+       centerPane.getChildren().remove(l); 
+    }
+    
+    /**
+     * Perform delete on all lines by clearing the line stack
+     * dysfunct because line stack no longer in use
+     */
+    /*
+    public void deleteAll()
+    {
+        while (!lineStack.empty())
+        {
+            Line l = lineStack.pop();
+            delete(l);
+        }
+    }   
+    */
+
+    /**
+     * On mouse click, get the first set of coordinates and snap them to grid
+     */
+    EventHandler<MouseEvent> press = (MouseEvent e) ->
+    {
+        //Specifies that depending on the location the line points are located
+        //the line ends will either snap to the higher or lower value divisible by 10
+        //10 was chosen instead of 50 for the line, for flexibility in the UML diagram
+        if (sidePanel.lineBtnToggled()) 
+        {
+            double x = e.getX();
+            double y = e.getY();
+          
+            if (x % 10 < 5) 
+            {
+                x1 = x - x % 10;
+            } 
+            else 
+            {
+                x1 = x + 10 - x % 10;
+            }
+            if (y % 10 < 5) 
+            {
+                y1 = y - y % 10;
+            }
+            else
+            {
+                y1 = y + 10 - y % 10;
+            }
+        }
+    };
+
+    /**
+     * On mouse release, get second set of coordinates
+     */
+    EventHandler<MouseEvent> release = (MouseEvent e) ->
+    {
+        if (sidePanel.lineBtnToggled()) 
+        {
+            double x = e.getX();
+            double y = e.getY();
+            if (x % 10 < 5)  {x2 = x - x % 10;} 
+            else {x2 = x + 10 - x % 10;}
+            if (y % 10 < 5) {y2 = y - y % 10;} 
+            else {y2 = y + 10 - y % 10;}
+            if (x2 < 0) {x2 = 0;}
+            if (y2 < 0) {y2 = 0;}
+            drawLine();
+        }
+    };
+
+    /**
+     * Delete a line when it is clicked on
+     */
+    EventHandler<MouseEvent> delete = (MouseEvent e) -> 
+    {
+        delete((Line) e.getSource());
+    };
+    
+    
+    public void setLineType(int n)
+    {
+        lineType = n;
+    }
+}
---
+++ myTopMenu.java
@@ -0,0 +1,241 @@
+package RidersOfWekmar;
+
+import java.io.File;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//myTopMenu is an object to add file options through drop down menus
+public class myTopMenu {
+
+	MenuBar menuBar = new MenuBar();
+	ArrayList<Node> nodes = new ArrayList<Node>();
+	TextBoxClass textBox;
+	String addingToFile = "";
+        mySidePanel sidePanel;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public MenuBar addMenuBar(Stage primary, mySidePanel sp) {
+                sidePanel = sp;
+                Pane centerPane = sidePanel.getCenterPane();
+		Menu menuFile = new javafx.scene.control.Menu("File");
+		MenuItem newFile = new MenuItem("New");
+		MenuItem open = new MenuItem("Open");
+		MenuItem save = new MenuItem("Save");
+
+		//creates a new stage/window to make a new file 
+		newFile.setOnAction((ActionEvent event) -> {
+			Stage secondStage = new Stage();
+			BorderPane border = new BorderPane();
+			Pane centerPane2 = new Pane();
+			centerPane2.getStyleClass().add("centerPane");
+			mySidePanel sidePanel2 = new mySidePanel(centerPane2);
+			myTopMenu bar = new myTopMenu();
+
+			// layout of editor
+			border.setLeft(sidePanel2.addSidePanel());
+                        border.setTop(bar.addMenuBar(secondStage, sidePanel2));
+			border.setCenter(centerPane2);
+
+			Scene secondScene = new Scene(border, 800, 800);
+			secondStage.setTitle("Riders of Wekmar Editor");
+			secondStage.setScene(secondScene);
+			secondScene.getStylesheets().add(RidersOfWekmar.class.getResource("application.css").toExternalForm());
+
+			secondStage.show();
+		});
+
+		//saves in a text file
+		save.setOnAction(new EventHandler<ActionEvent>() {
+			// We have a functional save aspect that will store node type and location to a
+			// text file, load function is planned but not functional
+			public void handle(ActionEvent t) {
+				FileChooser fc = new FileChooser();
+				fc.setTitle("Save Text");
+				nodes = getAllNodes(centerPane);
+
+				for (int i = 0; i < nodes.size(); i++) {
+					ArrayList<Double> list = new ArrayList<Double>();
+					list = getBounds(nodes.get(i));
+					System.out.println(i + " " + nodes.get(i).getId() + " " + list);
+					addingToFile = addingToFile + (i + " " + nodes.get(i).getId() + " " + list).toString() + "\n";
+
+				}
+
+				FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+				fc.getExtensionFilters().add(extFilter);
+				File file = fc.showSaveDialog(primary);
+
+				if (file != null) {
+					SaveFile(addingToFile, file);
+				}
+				addingToFile = "";
+			}
+		});
+
+		// exiting the program
+		MenuItem exit = new MenuItem("Exit", null);
+		exit.setOnAction(actionEvent -> Platform.exit());
+
+		// what is in the file tab
+		menuFile.getItems().addAll(newFile, open, save, exit);
+
+		// edit should contain the undos, redos and clear
+		Menu menuEdit = new javafx.scene.control.Menu("Edit");
+                MenuItem undo = new MenuItem("Undo");
+                undo.setOnAction((ActionEvent e) -> 
+                {
+                    sidePanel.fireUndoBtn();
+                });
+                
+                MenuItem redo = new MenuItem("Redo");
+                redo.setOnAction((ActionEvent e) -> 
+                {
+                    sidePanel.fireRedoBtn();
+                });
+                
+                MenuItem settings = new MenuItem("Settings");
+                settings.setOnAction((ActionEvent e) -> 
+                {
+                    
+                    Stage settingsStage = new Stage();
+                    settingsStage.setTitle("Settings");                    
+                    GridPane basePane = new GridPane(); 
+                    Scene settingsScene = new Scene(basePane, 300, 200);
+                    settingsStage.setScene(settingsScene);
+                    Text lineWidthText = new Text("Line width: ");
+                    TextField lineWidthField = new TextField();                    
+                    //HBox lineWidth = new HBox();
+                    //lineWidth.getChildren().addAll(lineWidthText, lineWidthField);
+                    //basePane.getChildren().add(lineWidth);
+                    
+                    Text testText = new Text("Test Text: ");
+                    TextField testField = new TextField();
+                    VBox texts = new VBox();
+                    VBox textFields = new VBox();
+                    texts.setAlignment(Pos.CENTER);
+                    textFields.setAlignment(Pos.CENTER);
+                    texts.getChildren().addAll(lineWidthText, testText);
+                    textFields.getChildren().addAll(lineWidthField, testField);
+                    //basePane.getChildren().addAll(texts, textFields);
+                    basePane.add(texts, 0,0);
+                    basePane.add(textFields, 1, 0);
+                    ColumnConstraints column1 = new ColumnConstraints();
+                    column1.setPercentWidth(50);
+                    ColumnConstraints column2 = new ColumnConstraints();
+                    column2.setPercentWidth(50);
+                    basePane.getColumnConstraints().addAll(column1, column2);
+                    settingsStage.show();
+                    
+                });
+                
+                menuEdit.getItems().addAll(undo, redo, settings);
+
+		// gives an information about the program
+		Menu menuHelp = new javafx.scene.control.Menu("About");
+		MenuItem about = new MenuItem("Welcome");
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About");
+		alert.setHeaderText("About The Riders Of Wekmar Editor");
+		String s = "This program is created to produce uml diagrams. There are resizale textboxes with three sections"
+				+ " name the class, instance variables and attributes. User has a choice of different lines to pick from to connect textboxes."
+				+ "\n\n"
+				+ "The main purpose to create the program was for a Software Engineering course by Ashley Camacho, Kailash Sayal, Kyle Marten, Peter Dawson and Samuel Aungst."
+				+ "The Students attend Millersville University.";
+		alert.setContentText(s);
+		about.setOnAction(actionEvent -> alert.show());
+		menuHelp.getItems().addAll(about);
+
+		menuBar.getMenus().addAll(menuFile, menuEdit, /* menuView, */ menuHelp);
+
+		return menuBar;
+
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	private void SaveFile(String content, File file) {
+		try {
+			FileWriter fileWriter = null;
+
+			fileWriter = new FileWriter(file);
+			fileWriter.write(content);
+			fileWriter.close();
+		} catch (IOException ex) {
+			Logger.getLogger(myTopMenu.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Fuction to return Arraylist of only top left corner of nodes
+	// intended use is for snapping Lines to text boxes within a predetermined
+	// proximity, currently not used
+	public static ArrayList<Double> getMinBounds(Node node) {
+		Bounds bounds;
+		ArrayList<Double> list = new ArrayList<Double>();
+		bounds = node.localToParent(node.getBoundsInLocal());
+		list.add(bounds.getMinX());
+		list.add(bounds.getMinY());
+		return list;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Returns the bounds for all nodes in scene, returning both the min
+	// and max X and Y coordinates too record both sides of lines and
+	// also because we can resize our text boxes we need the diagonal coordinates
+	// to determine location and size for save
+	public static ArrayList<Double> getBounds(Node node) {
+		Bounds bounds;
+		ArrayList<Double> list = new ArrayList<Double>();
+		bounds = node.localToParent(node.getBoundsInLocal());
+		list.add(bounds.getMinX());
+		list.add(bounds.getMinY());
+		list.add(bounds.getMaxX());
+		list.add(bounds.getMaxY());
+		return list;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Returns all nodes from "root" and descendents
+	public static ArrayList<Node> getAllNodes(Parent root) {
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		addAllDescendents(root, nodes);
+		return nodes;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Returns all descendents from parent node
+	private static void addAllDescendents(Parent parent, ArrayList<Node> nodes) {
+		for (Node node : parent.getChildrenUnmodifiable()) {
+			nodes.add(node);
+		}
+	}
+
+}
