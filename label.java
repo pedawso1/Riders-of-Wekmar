a1153ad648b4d72207cc9e07f6c712bbf86cdbab
knsayal <31737158+knsayal@users.noreply.github.com>
2017-12-02 19:37:07 -0500

    Update label.java

--- label.java
+++ label.java
@@ -115,6 +115,167 @@ public class label {
 		});
 
 	}
+	
+	 public static   void LoadText(String value,Double x,Double y,Pane box ){
+		 //////////////////////////////////////////////////////////////////////
+		 Label loadLabel = new Label(value);
+		 loadLabel.setLayoutX(x);
+		 loadLabel.setLayoutY(y);
+		 
+		 Stage window = new Stage();
+		 window.setTitle("Enter Label text");
+		 
+		 //Text for the text label
+		 TextArea lblInput =new TextArea("");
+		 lblInput.setMaxWidth(300);
+		 lblInput.setText(value);
+		 
+		 
+		 Label enterText = new Label("Enter Text:");
+		 Label enterFont = new Label("Font Size:");
+		 
+		 //Text for Fontsize
+		 TextField fontSize =new TextField("25");
+		 fontSize.setMaxWidth(50);
+		 
+		 
+		 
+		 VBox layout = new VBox(10);
+		 Group text = new Group();
+		 Button exit = new Button("Save and Close");
+		 layout.getChildren().addAll(enterText,lblInput,enterFont,fontSize,exit);
+		 Scene scene = new Scene(layout);
+		 
+		
+		  
+		 
+		  String StringFont = fontSize.getText();
+		  
+		  
+		  fontNumber =Integer.parseInt(StringFont);
+		 
+		  loadLabel.setFont(Font.font("Verdana",FontWeight.EXTRA_BOLD,fontNumber));
+		 
+		  text.getChildren().add(loadLabel);
+		  box.getChildren().add(text);
+		  
+		 
+		 
+		 
+		 window.setWidth(400);
+		 window.setHeight(400);
+		 window.setScene(scene);
+		 
+		
+		///////////////////////////////////////////////////////////////////// 
+		 
+		 exit.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+		 box.getChildren().remove(text);
+		 text.getChildren().remove(loadLabel);
+		  
+		  String StringValue = lblInput.getText();
+		  String StringFont2 = fontSize.getText();
+		  
+		  
+		  
+		  fontNumber =Integer.parseInt(StringFont2);
+		  loadLabel.setText(StringValue);
+		  loadLabel.setFont(Font.font("Verdana",FontWeight.EXTRA_BOLD,fontNumber));
+		  
+		 
+		  text.getChildren().add(loadLabel);
+		  box.getChildren().add(text);
+		  
+		 window.close();
+		 
+		 
+		 });
+		 
+	    ///////////////////////////////////////////////////////////////////////////////	
+		 
+		 
+		 text.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+
+	            orgSceneX = e.getSceneX();
+	            orgSceneY = e.getSceneY();
+                 System.out.println(orgSceneY);
+	            orgTranslateX = text.getTranslateX();
+	            orgTranslateY = text.getTranslateY();
+
+	         
+	        });
+		 
+		 
+		 
+		 ////////////////////////////////////////////////////////////////////////////////
+		 
+		 text.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> {
+
+	            double offsetX = e.getSceneX() - orgSceneX;
+	            double offsetY = e.getSceneY() - orgSceneY;
+	            double newTranslateX = orgTranslateX + offsetX;
+	            double newTranslateY = orgTranslateY + offsetY;
+
+	            text.setTranslateX(newTranslateX);
+	            text.setTranslateY(newTranslateY);
+	        });
+		 
+		 
+		/////////////////////////////////////////////////////////////////////////////////////////
+		 text.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+			 
+  
+			 if(e.getClickCount() == 2) 
+			 { 
+				
+				//text.getChildren().remove(label);
+			    window.show();
+			    
+			    
+					 
+			    
+					 
+					 
+					 
+					 
+					 
+			
+					 
+					 
+					
+			    
+			    
+			    
+			    
+			    
+			 } 
+			
+
+	         
+	        });
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 ////////////////////////////////////////////////////////////////////////
+		 
+	 }
+	 public double getFontSize(){
+		return fontNumber;
+		 
+		 
+	 }
+	 
+	 
+	 
+	
+	
+	
 }
 	 
 	
